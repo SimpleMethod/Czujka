9bd753715d5be903f6754ca562f5e833f05be911
SimpleMethod <michal.piotr.mlodawski@gmail.com>
2019-05-08 18:34:30 +0200

    Text correction in error descriptions

--- src/main/java/pl/simplemethod/czujka/botparser/StringParser.java
+++ src/main/java/pl/simplemethod/czujka/botparser/StringParser.java
@@ -7,7 +7,8 @@ public class StringParser {
     private final static String unsubscribeGlobal ="{\"response_type\":\"in_channel\",\"attachments\":[{\"type\":\"mrkdwn\",\"text\":\"Użytkownik *user_name* wypisał się z dzisiejszego zamykania biura :cry:\"}]}";
     private final static String signUpPerson = "{\"response_type\":\"ephemeral\",\"attachments\":[{\"type\":\"mrkdwn\",\"text\":\"Hura :smile: zapisałeś się do dzisiejszego zamykania biura o godzinie *close_hours*\"}]}";
     private final static String signUpGlobal = "{\"response_type\":\"in_channel\",\"attachments\":[{\"type\":\"mrkdwn\",\"text\":\"Hura :smile: użytkownik *user_name* zapisał się do dzisiejszego zamykania biura o godzinie *close_hours*\"}]}";
-    private final static String leavePerson = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Wypisałeś się z dzisiejszego zamykania biura.\"}}]";
+
+    private final static String leavePerson = "{\"response_type\":\"ephemeral\",\"attachments\":[{\"type\":\"mrkdwn\",\"text\":\"Wypisałeś się z dzisiejszego zamykania biura :cry:\"}]}";
     private final static String leavePenultimatePerson = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Zostałeś wybrany do zamykania biura :( Twój poprzednik się wypisał.\"}}]";
 
     private final static String leavePersonFound = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Nie jesteś już ostatnią osobą wychodzącą z biura, nie zamykasz ;)\"}}]";
@@ -15,11 +16,11 @@ public class StringParser {
     private final static String queuePerson = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Jesteś *user_que* w kolejce\"}}]";
 
     // Exceptions handlers
-    private final static String leavePersonNull = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Wystąpił błąd, prawdopodobnie nie byłeś zapisany do zamykania biura dzisiaj.\"}}]";
+    private final static String leavePersonNull = "{\"response_type\":\"ephemeral\",\"attachments\":[{\"type\":\"mrkdwn\",\"text\":\"Wystąpił błąd, prawdopodobnie nie byłeś zapisany do zamykania biura dzisiaj.\"}]}";
     private final static String leavePersonSaveNull = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Wystąpił błąd z bazą danych. Spróbuj ponownie :(\"}}]";
-    private final static String leavePersonEnty = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Wprowadzone dane są niepoprawne. Spróbuj ponownie! \\nPrawidłowy format godziny to \\\"HH:MM\\\".\"}}]";
+    private final static String leavePersonEntry = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Wprowadzone dane są niepoprawne. Spróbuj ponownie! \\nPrawidłowy format godziny to \\\"HH:MM\\\".\"}}]";
 
-    // Use only for global messages (API -> chat.)!
+    // Use only for global messages (API -> chat.)!Wypisałeś się z dzisiejszego zamykania biura.
     private final static String unsubscribeGlobalBlock = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Użytkownik *user_name* wypisał się z dzisiejszego zamykania biura :cry:\"}}]";
     private final static String signUpGlobalBlock = "[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Hura :smile: użytkownik *user_name* zapisał się do dzisiejszego zamykania biura o godzinie *close_hours*\"}}]";
 
@@ -28,8 +29,8 @@ public class StringParser {
         return unsubscribePerson;
     }
 
-    public String getLeavePersonEnty() {
-        return leavePersonEnty;
+    public String getLeavePersonEntry() {
+        return leavePersonEntry;
     }
 
     public String getLeavePersonSaveNull() {
--- src/main/java/pl/simplemethod/czujka/client/CzujkaController.java
+++ src/main/java/pl/simplemethod/czujka/client/CzujkaController.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.sql.Date;
 import java.sql.Time;
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -144,12 +145,12 @@ public class CzujkaController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
 
+        String channel = botController.getUserPrivateChannelID(user_name);
         //usuwa wszystkie znaki poza cyframi i :
         text = text.replaceAll("([^0-9:])+", "");
 
         if ((text.length() > 4 && text.charAt(2) != ':') || text.length() < 4) {
-            String channel = botController.getUserPrivateChannelID(user_name);
-            botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonEnty());
+            botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonEntry());
 
         } else if (text.length() == 4 && !text.contains(":")) {
             //dodaje : do parsowania czasu ze stringa
@@ -158,7 +159,15 @@ public class CzujkaController {
             text = builder.toString();
         }
 
-        Time user_time = Time.valueOf(LocalTime.parse(text));
+        //przechwytywanie
+        Time user_time = Time.valueOf(LocalTime.MIN);
+        try {
+            user_time = Time.valueOf(LocalTime.parse(text));
+        } catch (DateTimeException e) {
+            botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonEntry());
+            return new ResponseEntity<>(stringParser.getLeavePersonEntry(), headers, HttpStatus.BAD_REQUEST);
+        }
+
         Users find = repository.getUserByUsername(user_name);
         Users user = new Users(user_name, user_time);
 
@@ -169,7 +178,6 @@ public class CzujkaController {
                 repository.save(user);
             }
         } catch (Exception e) {
-            String channel = botController.getUserPrivateChannelID(user_name);
             botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonNull());
         }
 
@@ -180,15 +188,14 @@ public class CzujkaController {
             String penultimateUser = repository.getPenultimateUserInQue();
 
             if (penultimateUser != null) {
-                String channel = botController.getUserPrivateChannelID(penultimateUser);
+                channel = botController.getUserPrivateChannelID(penultimateUser);
                 botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonFound());
             }
 
-            String channel = botController.getUserPrivateChannelID(penultimateUser);
+            channel = botController.getUserPrivateChannelID(penultimateUser);
             botController.postRichChatMessage(channel, " ", stringParser.getLeavePersonAttend());
 
         } else {
-            String channel = botController.getUserPrivateChannelID(user_name);
             botController.postRichChatMessage(channel, " ", stringParser.getQueuePerson(Integer.toString(que + 1)));
         }
 
@@ -225,6 +232,6 @@ public class CzujkaController {
         String channel = botController.getUserPrivateChannelID(penultimateUser);
         botController.postRichChatMessage(channel, " ", stringParser.getLeavePenultimatePerson());
 
-        return new ResponseEntity<>(stringParser.getLeavePerson(), headers, HttpStatus.OK);
+        return new ResponseEntity<>(stringParser.getUnsubscribePerson(), headers, HttpStatus.OK);
     }
 }
